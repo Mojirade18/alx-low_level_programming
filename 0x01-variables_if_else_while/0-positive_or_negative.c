5c340b220467d983af757e8b7d70a2bd9bb1ed3b
Mojirade18 <mojiradeoluranti@gmail.com>
2023-08-13 05:33:48 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@
  * Return: Always (Success)
  *
  */
-		
+
 int main(void)
 
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
@@ -32,4 +32,4 @@ int main(void)
 	printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
