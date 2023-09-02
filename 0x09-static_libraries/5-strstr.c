c5ef0a3c893f36697dd0c44f98d589151499c100
Mojirade18 <mojiradeoluranti@gmail.com>
2023-09-01 17:34:38 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring in a string.
+ * @haystack: The string to search in.
+ * @needle: The substring to search for.
+ *
+ * Return: A pointer to the beginning of the located substring, or NULL
+ *if the substring is not found.
+ */
+
+
