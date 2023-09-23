6a2afd7d36dfa281d9b4ccb02e6f002de1669ce3
Mojirade18 <mojiradeoluranti@gmail.com>
2023-09-22 17:56:52 -0700

    the list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -15,7 +15,7 @@ size_t list_len(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-	for (i = 1; h->next != NULL; i++)
+	for (j = 1; h->next != NULL; j++)
 		h = h->next;
-	return (i);
+	return (j);
 }
