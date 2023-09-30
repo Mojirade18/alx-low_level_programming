902e1eef5ddcf735ce618744738fa76f95336390
Mojirade18 <mojiradeoluranti@gmail.com>
2023-09-29 17:36:02 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
+ * @number: number to print in binary
  */
 void print_binary(unsigned long int number)
 {
